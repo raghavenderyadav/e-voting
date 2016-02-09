d55916a3a11df81304012664202a9c2944e9c1f8
ak <ak@cryptobitex.com>
2016-02-09 12:40:16 +0300

    Objects model.

---
+++ sources/e-voting/e-voting-server/src/main/java/uk/dsxt/votingserver/VotingServerResource.java
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * e-voting system                                                            *
+ * Copyright (C) 2016 DSX Technologies Limited.                               *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied                         *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ *                                                                            *
+ * You can find copy of the GNU General Public License in LICENSE.txt file    *
+ * at the top-level directory of this distribution.                           *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+package uk.dsxt.votingserver;
+
+import lombok.extern.log4j.Log4j;
+import uk.dsxt.votingserver.datamodel.VoterJSON;
+import uk.dsxt.votingserver.datamodel.VotingJSON;
+import uk.dsxt.votingserver.datamodel.VotingRightJSON;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Log4j
+@Path("/voting-api")
+public class VotingServerResource {
+    private final VotingServerManager manager;
+
+    public VotingServerResource(VotingServerManager manager) {
+        this.manager = manager;
+    }
+
+    @POST
+    @Path("/votingRights")
+    @Produces("application/json")
+    public VotingRightJSON[] getVotingRights() {
+        return manager.getVotingRights();
+    }
+
+    @POST
+    @Path("/voters")
+    @Produces("application/json")
+    public VoterJSON[] getVoters() {
+        return manager.getVoters();
+    }
+
+    @POST
+    @Path("/voting")
+    @Produces("application/json")
+    public VotingJSON getVoting() {
+        return manager.getVoting();
+    }
+}
---
+++ sources/e-voting/e-voting-server/src/main/java/uk/dsxt/votingserver/datamodel/QuestionJSON.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * e-voting system                                                            *
+ * Copyright (C) 2016 DSX Technologies Limited.                               *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied                         *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ *                                                                            *
+ * You can find copy of the GNU General Public License in LICENSE.txt file    *
+ * at the top-level directory of this distribution.                           *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+package uk.dsxt.votingserver.datamodel;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Value;
+
+@Value
+public class QuestionJSON {
+    Integer id;
+    String question;
+    AnswerJSON[] answers;
+
+    @JsonCreator
+    public QuestionJSON(@JsonProperty("id") Integer id, @JsonProperty("question") String question, @JsonProperty("answers") AnswerJSON[] answers) {
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+    }
+}
