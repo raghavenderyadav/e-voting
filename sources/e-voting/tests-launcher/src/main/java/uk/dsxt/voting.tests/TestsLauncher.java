319deeb7aff065665eb8589e86db23d5d2154301
george.mavchun <gm@dsx.uk>
2016-02-13 13:12:42 +0300

    Tests launcher application added.

---
+++ sources/e-voting/tests-launcher/src/main/java/uk/dsxt/voting.tests/TestsLauncher.java
@@ -0,0 +1,13 @@
+package uk.dsxt.voting.tests;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class TestsLauncher {
+    public static void main(String[] args) {
+        log.debug("Starting Tests Launcher...");
+
+        // TODO Implement tests launch process.
+
+    }
+}
