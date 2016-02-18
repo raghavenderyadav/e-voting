225a633770ecb61a006d8c68f0517b30ff4791c2
George Mavchun <gm@dsxt.uk>
2016-02-18 12:40:58 +0300

    Path fixed in build_from_sources.sh

--- sources/e-voting/build_from_sources.sh
+++ sources/e-voting/build_from_sources.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 echo Updating git repository...
+git reset --hard
 git pull
 echo git repository updated
 gradle clean build
 echo Unzipping distribution
 unzip -o /home/ubuntu/e-voting/src/e-voting/sources/e-voting/tests-launcher/build/distributions/tests-launcher-0.1-SNAPSHOT.zip -d /home/ubuntu/e-voting/build
 echo Coping necessary jar files to libs folder
-cp /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/lib/client-0.1-SNAPSHOT.jar /home/ubuntu/e-voting/build/libs/client.jar
-cp /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/lib/nxt.jar /home/ubuntu/e-voting/build/libs/nxt.jar
+cp /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/lib/client-0.1-SNAPSHOT.jar /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/libs/client.jar
+cp /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/lib/nxt.jar /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/libs/nxt.jar
 echo Build process is finished successfully
