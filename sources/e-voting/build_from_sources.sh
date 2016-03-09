3e9f778f57cdc8852e0dd14dc94ecef424c9d61a
dz-dsxt <dz@dsxt.uk>
2016-03-09 15:58:40 +0300

    Improvement of update script.

--- sources/e-voting/build_from_sources.sh
+++ sources/e-voting/build_from_sources.sh
@@ -6,10 +6,16 @@ echo git repository updated
 gradle clean build
 echo Unzipping distribution
 unzip -o /home/ubuntu/e-voting/src/e-voting/sources/e-voting/tests-launcher/build/distributions/tests-launcher-0.1-SNAPSHOT.zip -d /home/ubuntu/e-voting/build
+mv /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/* /home/ubuntu/e-voting/build/;
 echo Coping necessary jar files to libs folder
-cp /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/lib/client-0.1-SNAPSHOT.jar /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/libs/client.jar
-cp /home/ubuntu/e-voting/src/e-voting/sources/e-voting/libs/nxt.jar /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/libs/nxt.jar
-cp /home/ubuntu/e-voting/src/e-voting/sources/e-voting/libs/nxt.jar /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/lib/nxt.jar
-cp -r /home/ubuntu/e-voting/src/e-voting/sources/e-voting/tests-launcher/src/main/resources/json/* /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/bin/json/
-cp -r /home/ubuntu/e-voting/src/e-voting/sources/e-voting/registries-server/src/main/resources/json/* /home/ubuntu/e-voting/build/tests-launcher-0.1-SNAPSHOT/bin/json/
+cp /home/ubuntu/e-voting/build/lib/client-0.1-SNAPSHOT.jar /home/ubuntu/e-voting/build/libs/client.jar
+cp /home/ubuntu/e-voting/src/e-voting/sources/e-voting/libs/nxt.jar /home/ubuntu/e-voting/build/libs/nxt.jar
+cp /home/ubuntu/e-voting/src/e-voting/sources/e-voting/libs/nxt.jar /home/ubuntu/e-voting/build/lib/nxt.jar
+cp -r /home/ubuntu/e-voting/src/e-voting/sources/e-voting/tests-launcher/src/main/resources/json/* /home/ubuntu/e-voting/build/bin/json/
+cp -r /home/ubuntu/e-voting/src/e-voting/sources/e-voting/registries-server/src/main/resources/json/* /home/ubuntu/e-voting/build/bin/json/
+echo build of frontend
+cp -r /home/ubuntu/e-voting/src/e-voting/sources/e-voting/gui-public/* /home/ubuntu/e-voting/build/gui-public/;
+cd /home/ubuntu/e-voting/build/gui-public/;
+npm update;
+bower update;
 echo Build process is finished successfully
