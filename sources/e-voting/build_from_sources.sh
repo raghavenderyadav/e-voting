0fd10857bffff8a40370e823bf93c6ef6da474a2
dz-dsxt <dz@dsxt.uk>
2016-03-09 20:11:31 +0300

    Update script for frontend.

--- sources/e-voting/build_from_sources.sh
+++ sources/e-voting/build_from_sources.sh
@@ -23,7 +23,7 @@ cd /home/ubuntu/e-voting/build/gui-public/;
 npm update;
 bower update;
 sed -i 's/base href=\"\/\"/base href=\"\/nsd\/\"/g' /home/ubuntu/e-voting/build/gui-public/app/index.html
-echo "angular.module('e-voting.server-properties', []).constant('serverProperties', {\"serverUrl\": \"http://localhost:9000/\", \"pathToApi\": \"nsd/api\" });" > /home/ubuntu/e-voting/build/gui-public/app/server-properties.js;
+echo "angular.module('e-voting.server-properties', []).constant('serverProperties', {\"serverUrl\": \"\/\", \"pathToApi\": \"nsd/api\" });" > /home/ubuntu/e-voting/build/gui-public/app/server-properties.js;
 cp -r /home/ubuntu/e-voting/build/gui-public/* /home/ubuntu/e-voting/build/gui-public1/;
 sed -i 's/base href=\"\/nsd\/\"\/\"/base href=\"\/nsd1\/\"/g' /home/ubuntu/e-voting/build/gui-public1/app/index.html
 echo "angular.module('e-voting.server-properties', []).constant('serverProperties', {\"serverUrl\": \"\/\", \"pathToApi\": \"nsd1/api\" });" > /home/ubuntu/e-voting/build/gui-public1/app/server-properties.js;
