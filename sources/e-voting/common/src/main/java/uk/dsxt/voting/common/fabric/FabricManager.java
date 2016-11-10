2196a1593f960b790311c326899e7b0a0dec3c78
Mikelle <mikhwall@gmail.com>
2016-11-10 22:34:29 +0300

    Did some refactoring

--- sources/e-voting/common/src/main/java/uk/dsxt/voting/common/fabric/FabricManager.java
+++ sources/e-voting/common/src/main/java/uk/dsxt/voting/common/fabric/FabricManager.java
@@ -47,50 +47,33 @@ public class FabricManager implements WalletManager {
     private final String memberServiceUrl;
     private final String keyValStore;
 
-    ChainCodeResponse deployResponse = null;
+    private ChainCodeResponse deployResponse;
     
     private Process fabricProcess;
     private Chain chain;
-    private boolean isInitialized = false;
-    private int id = 0;
+    private boolean isInitialized;
+    private int id;
 
-    // for testing purposes
-    private FabricManager() {
-        chainName = null;
-        admin = null;
-        passphrase = null;
-        peers = null;
-        memberServiceUrl = null;
-        keyValStore = null;
-    }
     
     public FabricManager(String chainName, String admin, String passphrase, String memberServiceUrl, String keyValStore, 
-                         List<String> peers) throws RegistrationException, EnrollmentException, CertificateException, InterruptedException {
+                         List<String> peers) {
         this.chainName = chainName;
         this.admin = admin;
         this.passphrase = passphrase;
         this.memberServiceUrl = memberServiceUrl;
         this.keyValStore = keyValStore;
         this.peers = peers;
-        chain = new Chain(chainName);
-        chain.setMemberServicesUrl(memberServiceUrl, null);
-        chain.setKeyValStore(new FileKeyValStore(System.getProperty("user.home") + keyValStore));
-        peers.forEach(peer -> chain.addPeer(peer, null));
-        Member registrar = chain.getMember(admin);
-        if (!registrar.isEnrolled()) {
-            registrar = chain.enroll(admin, passphrase);
-        }
-        chain.setRegistrar(registrar);
     }
 
     @Override
     public void start() {
 
         Runtime rt = Runtime.getRuntime();
-        FabricManager fabricManager = new FabricManager();
+        FabricManager fabricManager = new FabricManager(chainName, admin, passphrase, memberServiceUrl, keyValStore, peers);
         printOutput errorReported, outputMessage;
         
         try {
+
             fabricProcess = rt.exec("docker-compose up");
             
             isInitialized = true;
@@ -101,11 +84,36 @@ public class FabricManager implements WalletManager {
             errorReported.start();
             outputMessage.start();
 
-            TimeUnit.SECONDS.sleep(10);
+            sleep(20);
+            
+            chain = new Chain(chainName);
+            
+            try {
+                chain.setMemberServicesUrl(memberServiceUrl, null);
+            } catch (CertificateException e) {
+                e.printStackTrace();
+            }
+            chain.setKeyValStore(new FileKeyValStore(System.getProperty("user.home") + keyValStore));
+            peers.forEach(peer -> chain.addPeer(peer, null));
+            
+            Member registrar = chain.getMember(admin);
+            if (!registrar.isEnrolled()) {
+                try {
+                    registrar = chain.enroll(admin, passphrase);
+                } catch (EnrollmentException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            chain.setRegistrar(registrar);
+            
+            sleep(10);
             
             deployResponse = initChaincode();
+            
+            
 
-        } catch (IOException | EnrollmentException | RegistrationException | InterruptedException e) {
+        } catch (IOException | EnrollmentException | RegistrationException  e) {
             e.printStackTrace();
         }
     }
@@ -165,24 +173,16 @@ public class FabricManager implements WalletManager {
         request.setArgs(new ArrayList<>(Arrays.asList("write" , Integer.toString(id), new String(body, StandardCharsets.UTF_8))));
         request.setChaincodeID(deployResponse.getChainCodeID());
         request.setChaincodeName(deployResponse.getChainCodeID());
-        Member member = null;
-        try {
-            member = getMember("User1", "chain");
-        } catch (RegistrationException | EnrollmentException e) {
-            e.printStackTrace();
-        }
+        
+        Member member = getMember("User1", "chain");
 
         try {
             member.invoke(request);
         } catch (ChainCodeException e) {
             e.printStackTrace();
         }
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        sleep(10);
+        
         id++;
         
         return null;
@@ -191,11 +191,24 @@ public class FabricManager implements WalletManager {
     @Override
     public List<Message> getNewMessages(long timestamp) {
         
+        List<Message> result = new ArrayList<>();
+        
         for (int i = 0; i <= id; i++) {
-            System.out.println(getNewMessage(i).getMessage());
+            Message message = new Message(Integer.toString(id), 
+                                getNewMessage(i).getMessage().getBytes(), 
+                                true);
+            result.add(message);
         }
         
-        return null;
+        result.forEach(message -> {
+            try {
+                System.out.println(new String(message.getBody(), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        return result;
     }
 
     private ChainCodeResponse getNewMessage(int id) {
@@ -203,18 +216,9 @@ public class FabricManager implements WalletManager {
         request.setArgs(new ArrayList<>(Arrays.asList("read", Integer.toString(id))));
         request.setChaincodeID(deployResponse.getChainCodeID());
         request.setChaincodeName(deployResponse.getChainCodeID());
-        Member member = null;
-        try {
-            member = getMember("User1", "chain");
-        } catch (RegistrationException | EnrollmentException e) {
-            e.printStackTrace();
-        }
+        Member member = getMember("User1", "chain");
         
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep(10);
 
         try {
             return member.query(request);
@@ -224,17 +228,33 @@ public class FabricManager implements WalletManager {
         return null;
     }
 
-    private Member getMember(String enrollmentId, String affiliation) throws RegistrationException, EnrollmentException {
+    private Member getMember(String enrollmentId, String affiliation) {
         Member member = chain.getMember(enrollmentId);
         if (!member.isRegistered()) {
             RegistrationRequest registrationRequest = new RegistrationRequest();
             registrationRequest.setEnrollmentID(enrollmentId);
             registrationRequest.setAffiliation(affiliation);
-            member = chain.registerAndEnroll(registrationRequest);
+            try {
+                member = chain.registerAndEnroll(registrationRequest);
+            } catch (RegistrationException | EnrollmentException e) {
+                e.printStackTrace();
+            }
         } else if (!member.isEnrolled()) {
-            member = chain.enroll(enrollmentId, member.getEnrollmentSecret());
+            try {
+                member = chain.enroll(enrollmentId, member.getEnrollmentSecret());
+            } catch (EnrollmentException e) {
+                e.printStackTrace();
+            }
         }
         return member;
+    }
+    
+    private static void sleep(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     
     public static void main(String[] args) throws RegistrationException, CertificateException, InterruptedException, EnrollmentException {
@@ -243,19 +263,18 @@ public class FabricManager implements WalletManager {
         FabricManager fabricManager = new FabricManager("chain", "admin", "Xurw3yU9zI0l", "grpc://172.17.0.1:7054", 
             "/test2.properties", peers);
         
-        fabricManager.start();  
-        try {
-            Thread.sleep(10000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
+        fabricManager.start();
+        
+        sleep(10);
+        
         fabricManager.sendMessage("Hello".getBytes());
         
         fabricManager.sendMessage("How are you?.".getBytes());
         
+        fabricManager.sendMessage("I'm fine".getBytes());
         fabricManager.getNewMessages(10);
 
-        TimeUnit.SECONDS.sleep(10);
+        sleep(10);
         
         //fabricManager.stop();
     }
